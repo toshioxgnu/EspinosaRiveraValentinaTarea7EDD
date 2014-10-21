b3c7dd9e70ec50f07429c1d56b6cc25848567981
Jose <jozho.g@gmail.com>
2014-10-21 10:34:53 -0300

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Cabecera.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Inicio Programa" << endl;
+    cout<<"======MENU DEL SISTEMA======"<<endl;
+    cout<<"a) Generar los n caballeros de la mesa redonda"<<endl;
+    cout<<"b) Mostrar la informacion contenida en la mesa redonda"<<endl;
+    cout<<"c) Solicitar k y elegir el nombre del caballero elegido"<<endl;
+    cout<<"d) Salir"<<endl;
+    char op;
+    cout<<">>>>>>";cin>>op;
+
+    switch (op){
+        case 'a':break;
+        case 'b':break;
+        case 'c':break;
+        case 'd':break;
+    }
+    return 0;
+}
